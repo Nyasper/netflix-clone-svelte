b1a98b62f66719a1156e0c46f2a6fdba42fd91b4
Nyasper <herreragonzalo39@gmail.com>
2025-12-23 08:11:57 -0300

    header_finish

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Header from "$lib/components/Header.svelte";
+
+	let state = $state(false);
+	const changeState = () => {
+		state = !state;
+	};
+</script>
+
+
+<Header />
+<h1>Main Page</h1>
+
+<div class={`a ${state ? '' : ''}`}>
+	a
+</div>
+<div class={['b', {'replace': state}]}>
+	b
+</div>
+
+<button onclick={changeState}>Change State ({state})</button>
+
+<style>
+	:global(body) {
+		min-height: 200vh;
+	}
+
+
+	h1 {
+		color: white;
+		font-size: 3em;
+		text-align: center;
+		margin-top: 2rem;
+	}
+	button {
+		margin-top: 2rem;
+		padding: 0.5rem 1rem;
+		font-size: 1rem;
+		background-color: white;
+		cursor: pointer;
+	}
+
+	.a {
+		width: 100px;
+		height: 100px;
+		background-color: white;
+	}
+	.b {
+		width: 100px;
+		height: 100px;
+		background-color: grey;
+	}
+
+	.replace {
+		background-color: yellow;
+	}
+</style>
+
+
